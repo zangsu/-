8379d361b65d01b916a3fdf4f82786174b7f8c33
zangsu <zangsu_@naver.com>
2022-05-02 22:44:54 +0900

    2. 프린터 solve

--- 코딩테스트 고득점 Kit/스택, 큐/src/problem2.java
+++ 코딩테스트 고득점 Kit/스택, 큐/src/problem2.java
@@ -1,54 +1,52 @@
 import java.util.*;
 public class problem2 {
     public static int solution(int[] priorities, int location) {
-        int target = priorities[location];//타겟의 중요도.
-        int[] dup = new int[9];//각 중요도 별 작업개수 저장
-        int FirstJob = 0;//나보다 중요도가 높은 작업들의 개수 저장
-        int FirstLocation = location;//target의 첫 위치
-        int turn = 0;
         Queue<Integer> queue = new LinkedList<Integer>();
-        for(int i : priorities){
+        int turn = 0;
+        int[] dup = new int[9];
+        int MVP = 0;//제일 먼저 수행해야 하는 작업
+        int target = priorities[location];
+        for(int i : priorities) {
             queue.add(i);
             dup[i-1]++;
         }
-        for(int i = target; i<9; i++)
-            FirstJob += dup[i];
-        for(int i = 0; i<priorities.length; i++)
+        for(int i = 8; i>=0; i--)
         {
-            int queueHead = queue.poll();
-            if(FirstJob != 0) {//중요도 높은 게 있는경우
-                if (queueHead > target) {
-                    turn++;
-                    FirstJob--;
-
-                    if (i < FirstLocation)
-                        location--;
-                }
-                else
-                    queue.add(queueHead);
-            }
-            else{//중요도 높은게 없는경우
-                if(queueHead== target){
-                    if(i != FirstLocation) {
-                        turn++;
-//                        if(i < FirstLocation)
-//                            location--;
-                    }
-                    else
-                        return turn+1;
-                }
-                else{
-                    queue.add(queueHead);
-                }
+            if(dup[i] != 0) {
+                MVP = i + 1;
+                break;
             }
         }
-        for(int i = 0; i<location; i++)
-        {
-            if(queue.isEmpty())
-                break;
+        while(!queue.isEmpty()){
             int queueHead = queue.poll();
-            if(queueHead == target)
-                turn++;
+            if(queueHead == MVP){
+                if(location != 0){
+                    location--;
+                    turn++;
+                    dup[MVP-1]--;
+                    if(dup[MVP-1] == 0)
+                    {
+                        for(int i = MVP-2; i>=0; i--)
+                        {
+                            if(dup[i] != 0) {
+                                MVP = i + 1;
+                                break;
+                            }
+                        }
+                    }
+                }//출력이 target이 아닌경우
+                else//target이 출력 될 경우
+                {
+                    return turn+1;
+                }
+            }
+            else{
+                if(location == 0)
+                    location = queue.size();
+                else
+                    location--;
+                queue.add(queueHead);
+            }
         }
         return turn+1;
     }
