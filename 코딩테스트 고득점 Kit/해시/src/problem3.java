7064180bb8a77dd84bbb1627b0e04f67150281e9
zangsu <zangsu_@naver.com>
2022-05-07 12:49:19 +0900

    위장 100/100

--- 코딩테스트 고득점 Kit/해시/src/problem3.java
+++ 코딩테스트 고득점 Kit/해시/src/problem3.java
@@ -1,2 +1,19 @@
+//위장
+import javax.swing.text.html.HTMLDocument;
+import java.util.*;
+
 public class problem3 {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        HashMap<String, Integer> clo = new HashMap<String, Integer>();
+        for(String[] s : clothes){
+            if(!clo.containsKey(s[1]))
+                clo.put(s[1], 1);
+            else
+                clo.replace(s[1], clo.get(s[1])+1);
+        }
+        for(Integer i : clo.values())
+            answer*= i+1;
+        return answer-1;
+    }
 }
