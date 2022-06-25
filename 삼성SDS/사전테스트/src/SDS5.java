5bb6ea879a1647c541cb04c09130af8875f39287
zangsu <zangsu_@naver.com>
2022-06-25 17:41:59 +0900

    SDS5다시푸는중

--- 삼성SDS/사전테스트/src/SDS5.java
+++ 삼성SDS/사전테스트/src/SDS5.java
@@ -30,64 +30,48 @@ public class SDS5 {
                 map[row] = Input.split("");
             }//여기까지 입력
 
-            //문제 풀이
-            Queue<point> queue = new LinkedList<point>();
-            queue.add(new point(start_row, start_col));//시작 위치를 queue에 삽입
-            long ans = 0;
-            for(int path_len = 0; path_len<move_max; path_len++){
-                int thisTimeLen = queue.size();
-                for(int loopTime = 0; loopTime<thisTimeLen; loopTime++){
-                    point now = queue.poll();
-
-                    //상하이동 먼저 고려
-                    if(now.row == 0)
-                        ans++;
-                    else{
-                        if(!map[now.row-1][now.col].equals("X")) {
-                            queue.add(new point(now.row - 1, now.col));
-                        }
-                    }//여기까지가 상하이동
-                    if (now.row == row_max - 1) {
-                        ans++;
+            //데이터 전처리
+            Block[][] map_prev = new Block[row_max][];
+            Block[][] map_next = new Block[row_max][];
+            for (int row = 0; row < row_max; row++) {
+                map_prev[row] = new Block[col_max];
+                map_next[row] = new Block[col_max];
+                for(int col = 0; col<col_max; col++){
+                    if(row == 0 || row == row_max-1)
+                    {
+                        if (col == 0 || col == col_max - 1)
+                            map_prev[row][col] = new Block(0, 0, "Corner");
+                        else
+                            map_prev[row][col] = new Block(0, 0, "Side");
                     }
-                    else{
-                        if(!map[now.row+1][now.col].equals("X")) {
-                            queue.add(new point(now.row + 1, now.col));
-                        }
-                    }//여기까지가 상하이동
-
-                    //좌우이동 고려
-                    if(now.col == 0)
-                        ans++;
-                    else{
-                        if(!map[now.row][now.col-1].equals("X")) {
-                            queue.add(new point(now.row, now.col - 1));
-                        }
-                    }
-                    if (now.col == col_max - 1) {
-                        ans++;
-                    }
-                    else{
-                        if(!map[now.row][now.col+1].equals("X")) {
-                            queue.add(new point(now.row, now.col + 1));
-                        }
-                    }//여기까지가 좌우이동
-
-                    //System.out.printf("%d %d\n",now.row, now.col);
+                    else
+                        map_prev[row][col] = new Block(0, 0);
                 }
-            }//각 이동횟수별로 queue안의 위치를 꺼내 이동함(bfs)
-            System.out.printf("#%d %d\n", test+1, ans%1000000007);
+            }
+
+            //문제 풀이
+
+
         }//여기까지가 한 테스트케이스
 
     }
 }
-class point{
-    int row;
-    int col;
-    point(int x, int y){
-        this.row = x;
-        this.col = y;
+class Block{
+    int len;
+    int path_num;
+    String loc = "";//in, side, corner로 구분
+    Block(int x, int y){
+        this.len = x;
+        this.path_num = y;
+        this.loc = "in";
+    }
+    Block(int x, int y, String loc){
+        this.len = x;
+        this.path_num = y;
+        this.loc = loc;
     }
 
+
 }
+
 
